d69d3aa5bf2f718dabc19080ab585dc6484b6ab0
Alexey Yurchenko <alexes.dev@gmail.com>
2022-12-17 19:53:58 -0300

    feat(content): add a tgchannels page

---
+++ tools/README.md
@@ -0,0 +1,4 @@
+Update `data/full/tgchannels.yaml`:
+```
+SKIP_IMAGES=true TG_APP_ID=xxx TG_APP_HASH=xxx python3 tools/gettelegramchannels.py
+```
---
+++ tools/gettelegramchannels.py
@@ -0,0 +1,37 @@
+import telethon
+import os
+import io
+import yaml
+import PIL
+
+client = telethon.TelegramClient('hugo', os.getenv('TG_APP_ID'), os.getenv('TG_APP_HASH'))
+
+async def main():
+    with open("data/tgchannels.yaml", "r") as sourceFile:
+            items = yaml.safe_load(sourceFile)
+            res = []
+
+            for item in items:
+                ch = await client.get_entity(item['name'])
+                data = await client(telethon.tl.functions.channels.GetFullChannelRequest(channel=ch))
+
+                # save image
+                img_url = '/tgchannels/{}.jpg'.format(data.full_chat.id)
+                img_path = 'static' + img_url
+
+                if os.getenv('SKIP_IMAGES') == '':
+                    img = await client.download_media(data.full_chat.chat_photo, file=bytes)
+                    PIL.Image.open(io.BytesIO(img)).resize((128, 128)).save(img_path)
+
+                res.append(item | {
+                    'id': data.full_chat.id,
+                    'title': data.chats[0].title,
+                    'about': data.full_chat.about,
+                    'imageURL': img_url,
+                })
+
+            with open("data/full/tgchannels.yaml", "w") as f:
+                yaml.dump(res, f)
+
+with client:
+    client.loop.run_until_complete(main())
