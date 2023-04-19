21350dd948ec81a59579fec707086fdbbeef265e
Yuriy Bahur <yuriybahur4@gmail.com>
2023-04-20 01:06:39 +0300

    small fix: tools/loadTelegraphs.js

--- tools/loadTelegraphs.js
+++ tools/loadTelegraphs.js
@@ -156,7 +156,7 @@ Object.keys(config).forEach((dir) => {
   config[dir].forEach(async (configItem) => {
     try {
       const telegraphPagePath = getPathFromUrl(configItem.url);
-      const filePath = `content/${dir}/${configItem.slug ?? telegraphPagePath}.md`;
+      const filePath = `content/${dir}/${configItem.slug || telegraphPagePath}.md`;
 
       if (!fs.existsSync(filePath)) {
         const telegraphPage = await getTelegraphPage(telegraphPagePath);
