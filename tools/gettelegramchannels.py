92cddc19818f8e36d352b286238c7c75e7581d97
Alexey Yurchenko <alexes.dev@gmail.com>
2023-04-28 09:22:40 -0300

    feat: add new tgchannel

--- tools/gettelegramchannels.py
+++ tools/gettelegramchannels.py
@@ -12,6 +12,8 @@ async def main():
             res = []
 
             for item in items:
+                print(item['name'])
+
                 ch = await client.get_entity(item['name'])
                 data = await client(telethon.tl.functions.channels.GetFullChannelRequest(channel=ch))
 
@@ -20,8 +22,11 @@ async def main():
                 img_path = 'static' + img_url
 
                 if os.getenv('SKIP_IMAGES') == None or not os.path.exists(img_path):
-                    img = await client.download_media(data.full_chat.chat_photo, file=bytes)
-                    PIL.Image.open(io.BytesIO(img)).resize((128, 128)).save(img_path)
+                    try:
+                        img = await client.download_media(data.full_chat.chat_photo, file=bytes)
+                        PIL.Image.open(io.BytesIO(img)).resize((128, 128)).save(img_path)
+                    except Exception as e:
+                        print(e)
 
                 res.append(item | {
                     'id': data.full_chat.id,
