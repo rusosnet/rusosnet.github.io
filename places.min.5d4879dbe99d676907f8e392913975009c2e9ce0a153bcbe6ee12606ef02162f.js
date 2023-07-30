d0fcd252ff663e24ff8c0d6ef25b664a7f3d05de
alexesDev <alexesDev@users.noreply.github.com>
2023-07-30 02:43:52 +0000

    deploy: 2467ae0287422c70542151802f3f28e6b39d7437

---
+++ places.min.5d4879dbe99d676907f8e392913975009c2e9ce0a153bcbe6ee12606ef02162f.js
@@ -0,0 +1,31 @@
+(()=>{var t=[{coords:[-34.8958726,-56.1844382],description:`Tam srednij opt, cenyi na 20-40% nizhe, chem v roznice. Oplata toljko beznal.
+`,name:"Optovyij magazin Conaprole"},{coords:[-34.9013251,-56.139602],description:`1400 peso v mesyac. Mozhno oplachivatj pomesyachno.
+`,name:"We Fitness"},{coords:[-34.9106619,-56.1519385],description:`Ochenj neplokhoj restoran s mestnoj kukhnej
+`,name:"Valerio"},{coords:[-34.8987599,-56.13818239999999],description:"Odin iz dvukh glavnyikh magazinov s gejmerskoj tekhnikoj. Za mosjhnyim PK - syuda.\xA0",name:"Thot Computaci\xF3n"},{coords:[-34.9084417,-56.145797],description:`Na vkhode stoit lama. Ochenj vkusnyij i simpatichnyij restoran kstati. Lyudi v nyom rabotayut ochenj priyatnyiye.
+Seviche ne berite. Nu ili tak - siljno na lyubitelya. Kislyatina nevozmozhnaya.
+`,name:"Sushi Wok Per\xFA"},{coords:[-34.9030748,-56.15381449999999],description:`Otlichnyij kofe
+`,name:"Seis Montes Tostadores de caf\xE9"},{coords:[-34.9240097,-56.1584933],description:`Srednij TC. Yesli so vkhoda idti pryamo, to za eskalatorami najdete Claro i kupite simku + aktivaciya + popolneniye.
+`,name:"Punta Carretas Shopping"},{coords:[-34.9136236,-56.1539126],description:`Mozhno sdelatj:
+spravku o nesudimosti s konsuljskoj pechatjyu na ispanskom za $35 Algoritm oplatyi spravki:
+Vzyatj nomer scheta u sotrudnika konsuljstva Dojti do Red Pagos V terminale u vkhoda nazhatj perevod na schet banka XXXX (TODO) i vbitj nomer scheta Otdatj nomerok i denjgi na kasse FAQ Ya mogu zakazatj spravku o nesudimosti s apostilem na ispanskom yazyike? Kakiye dannyiye nuzhnyi budut i kak eto oplachivayetsya?
+Vam nuzhno lichno prijti v konsuljstvo i zapolnitj blank zayavleniya, pri sebe imetj pasporta — zagran. i vnutrennij. Poluchitj spravku na ispanskom yazyike mozhno budet primerno cherez 10 dnej. Stoimostj 35 doll. Myi rabotayem — ponedeljnik,sreda, pyatnica s 9-00 do 12-00.
+`,name:"Posoljstvo i konsuljstvo RF"},{coords:[-34.9090834,-56.148068],description:`Krajne posredstvennaya picceriya. Ne rekomenduyu
+`,name:"Pizzer\xEDa Trouville"},{coords:[-34.90225119999999,-56.1442164],description:`Zoopark i detskiye plosjhadki
+`,name:"Park Amistad-Espasio Viljya Dolores"},{coords:[-34.9049691,-56.1608965],description:"Yesjhyo otlichnoye kraftovoye pivo\xA0",name:"Oso Pardo"},{coords:[-34.9093571,-56.1477535],description:`norm shaurma ot nativnyikh armyan.
+govoryat po-russki
+`,name:"Nurr"},{coords:[-34.902612,-56.16098599999999],description:"Za tekhnikoj Apple - cyuda. Yestj yesjhyo otdeleniya.\xA0",name:"MundoMac"},{coords:[-34.9171401,-56.1588283],description:`Khoroshij fudkord bez TC. Raznoobraznyiye kukhni. Vkusnyij kofe.
+`,name:"Mercado Williman"},{coords:[-34.9096971,-56.14575259999999],description:"zanyatiya dlya detej ispanskim po voskresenjyam s 12 do 14. sejchas v gruppe 3 rebenka 6-7 let\xA0",name:"menteum (Centro intelectual para ni\xF1@s)"},{coords:[-34.91307069999999,-56.1574707],description:`Khoroshaya pivnaya
+`,name:"MBC"},{coords:[-34.9086445,-56.17250379999999],description:`Dlya birgikov luchsheye pivo i otlichnaya picca
+`,name:"Malafama"},{coords:[-34.7945295,-56.3321218],description:`Otlichnyij park/zoopark. Mozhno provesti celyij denj, sdelatj parizhu.
+`,name:"Lekokk"},{coords:[-34.9026438,-56.18289559999999],description:"Odin iz dvukh topovyikh magazinov s gejmerskoj tekhnikoj. Za mosjhnyim PK - syuda. Mogut privezti chto-nibudj edakoye pod zakaz, yesli dogovoritjsya.\xA0",name:"Laaca Gaming & Tecnolog\xEDa"},{coords:[-34.9096905,-56.159337],description:`Khoroshij myasnoj restoran v Montevideo po rekomendacii iz chata.
+`,name:"La barra 1114"},{coords:[-34.9057225,-56.2106872],description:`Nedavno otkryilisj. Yesjhyo odna dostojnaya pivovarnya
+`,name:"Indica bar"},{coords:[-34.9127952,-56.1637523],description:`Igrali tut v nastolki. Yestj stolyi vnutri i na ulice. Kukhnya otkryivayetsya posle 20. Obyichno mnogo naroda i nuzhno bronirovatj.
+`,name:"Enriqueta"},{coords:[-34.9084053,-56.1638306],description:"Mogut obmenyatj USDT na $:\n3% ot $1k 2% ot $5k Znayut anglijskij i otvechayut v TG: https://t.me/criptala\n",name:"Criptala"},{coords:[-34.9054978,-56.1973641],description:`Kotokafe
+`,name:"Cafeline"},{coords:[-34.9103245,-56.15276069999999],description:"Uyutnoye kafe. Neplokhoj kofe.\xA0",name:"Caf\xE9 La Latina"},{coords:[-34.9047845,-56.20510959999999],description:`Kofejnya v zdanii staroj apteki, interesnyij interjyer, vkusnyij kofe, norm cenyi. Yestj yeda, v osnovnom zavtraki i branchi
+`,name:"Cafe La Farmacia"},{coords:[-34.9223249,-56.1553116],description:`brali zdesj avto v arendu.
+- 80$/sutki, yesli net rabochej kreditki, nalichnyimi zalog 900$.
+organichenij na kilometrazh net, yezdili daleko.
+glavnyij menedzher govorit na inglishe, ryadovyiye net.
+po platnyim dorogam yezdili cherez distancionnuyu oplatu so schyota, potom prosto iz depozita vyichli. v celom, norm ekspiriyens
+`,name:"Autonom\xEDa Rent A Car"},{coords:[-34.90698399999999,-56.1902578],description:`Luchshaya shaverma
+`,name:"Ashot Shawarma"}],e=e=>({lat:e[0],lng:e[1]});window.initMap=function(){const a=e([-34.90798399999999,-56.1702578]),n=new google.maps.Map(document.getElementById("map"),{zoom:13,center:a}),s=new google.maps.InfoWindow,i=[];t.forEach(t=>{const o=new google.maps.Marker({position:e(t.coords),map:n});i.push(o),google.maps.event.addListener(o,"click",()=>{s.setContent(t.description.replace(/\n/,"<br />")),s.open(n,o)})});const o=document.querySelectorAll("a[data-jump]");for(let a=0;a<o.length;++a){const c=o[a].dataset.jump,r=t[c];o[a].addEventListener("click",t=>{t.preventDefault(),t.stopPropagation(),s.setContent(r.description.replace(/\n/,"<br />")),s.open(n,i[a]),n.setZoom(15),n.setCenter(e(r.coords)),window.scrollTo({top:0,behavior:"smooth"})})}}})()
